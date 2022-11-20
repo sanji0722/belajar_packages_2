f89e2896717b6ac1b39c926f5c413cb9fc0afe58
SANJI0722 <saepullohepul565@gmail.com>
2022-11-20 21:12:10 +0700

    belajar dart package

---
+++ example/belajar_packages_example.dart
@@ -0,0 +1,6 @@
+import 'package:belajar_packages/belajar_packages.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/belajar_packages.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library belajar_packages;
+
+export 'src/belajar_packages_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
